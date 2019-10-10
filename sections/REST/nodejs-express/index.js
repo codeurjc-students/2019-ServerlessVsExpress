408c2395e1a0f2fe82f764a06964afd87b916761
Francisco Robles Castro <franrobles8@gmail.com>
2019-10-10 19:09:33 +0200

    Added update and delete methods and modified some parts of the code

--- sections/REST/nodejs-express/index.js
+++ sections/REST/nodejs-express/index.js
@@ -15,12 +15,11 @@ app.get('/', (req, res) => {
 app.get('/users', (req, res) => {
     services.getAllUsers()
     .then((users) => {
-        console.log(users);
         res.send({
             error: false,
             code: 200,
             message: 'Users fetched successfully',
-            content: users
+            data: users
         });
     })
     .catch((err) => {
@@ -33,10 +32,31 @@ app.get('/users', (req, res) => {
     
 });
 
-app.post('/users/add', (req, res) => {
+app.get('/user/:username', (req, res) => {
+    services.getUser(req.params.username)
+    .then((user) => {
+        res.send({
+            error: false,
+            code: 201,
+            message: `${user.username} has been found!`,
+            data: user
+        });
+    })
+    .catch((err) => {
+        res.send({
+            error: true,
+            code: 200,
+            message: err
+        });
+    });
+});
+
+app.post('/user', (req, res) => {
     const user = {
         username: req.body.username,
-        password: req.body.surname
+        password: req.body.surname,
+        name: req.body.name,
+        lastname: req.body.lastname
     };
 
     services.addUser(user)
@@ -55,6 +75,64 @@ app.post('/users/add', (req, res) => {
         });
     });
     
+});
+
+app.put('/user', (req, res) => {
+    const user = {
+        username: req.body.username,
+        password: req.body.surname,
+        name: req.body.name,
+        lastname: req.body.lastname
+    };
+
+    services.updateUser(user)
+    .then((user) => {
+        res.send({
+            error: false,
+            code: 200,
+            message: `${user.username} updated successfully!`
+        });
+    })
+    .catch((err) => {
+        res.send({
+            error: true,
+            code: 200,
+            message: err
+        });
+    });
+    
+});
+
+app.delete('/user', (req, res) => {
+    const user = {
+        username: req.body.username
+    };
+
+    services.deleteUser(user)
+    .then((user) => {
+        res.send({
+            error: false,
+            code: 200,
+            message: `${user.username} deleted successfully!`
+        });
+    })
+    .catch((err) => {
+        res.send({
+            error: true,
+            code: 200,
+            message: err
+        });
+    });
+    
+});
+
+// If a route doesn't exist, use this middleware to show a 404 error message
+app.use((req, res, next) => {
+    res.status(404).send({
+        error: true,
+        code: 404,
+        message: "Url not found!"
+    });
 });
 
 // Exposing Server in port 3000
--- sections/REST/nodejs-express/mockup-services.js
+++ sections/REST/nodejs-express/mockup-services.js
@@ -1,20 +1,36 @@
+// Passwords should be encrypted to avoid security risks. This is just a demo.
 let users = [
     {
         username: "franrobles",
-        password: "1234"
+        password: "1234",
+        name: "Francisco",
+        lastname: "Robles"
     },
     {
         username: "anajohnson",
-        password: "1234"
+        password: "1234",
+        name: "Ana",
+        lastname: "Johnson"
     }
 ];
 
 // This is a DB simulation. Data should be managed with a real database inside functions.
 
 const getAllUsers = () => {
-    let usersWithoutPass = users.map(e => e.username);
+    let usersWithoutPass = users.map(e => ({
+        username: e.username,
+        name: e.name,
+        lastname: e.lastname
+    }));
     return new Promise((resolve, reject) => {
-        users ? resolve(usersWithoutPass) : reject("Error fetching users");
+        users ? resolve(usersWithoutPass) : reject("There aren't any users!");
+    });
+};
+
+const getUser = (username) => {
+    let user = users.find(e => e.username === username);
+    return new Promise((resolve, reject) => {
+        user ? resolve(user) : reject(`User ${username} not found!`);
     });
 };
 
@@ -24,13 +40,34 @@ const addUser = (user) => {
         users.push(user);
         added = true;
     }
-    console.log(added);
+
     return new Promise((resolve, reject) => {
-        (added) ? resolve(user) : reject("Error adding user...");
+        (added) ? resolve(user) : reject("User already exists!");
+    });
+};
+
+const updateUser = (user) => {
+    let index = users.findIndex(e => e.username === user.username);
+    users[index] = user;
+
+    return new Promise((resolve, reject) => {
+        (index !== -1) ? resolve(user) : reject(`User ${user.username} doesn't exist!`);
+    });
+};
+
+const deleteUser = (user) => {
+    let usersLengthBefore = users.length;
+    users = users.filter(e => e.username !== user.username);
+
+    return new Promise((resolve, reject) => {
+        (users.length < usersLengthBefore) ? resolve(user) : reject(`User ${user.username} doesn't exist!`);
     });
 };
 
 module.exports = {
     getAllUsers,
-    addUser
+    getUser,
+    addUser,
+    updateUser,
+    deleteUser
 };
