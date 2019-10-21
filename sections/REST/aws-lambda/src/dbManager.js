44abea3667e3f435a36d4b3fbb53acd96c7ad4e5
Francisco Robles Castro <franrobles8@gmail.com>
2019-10-21 11:01:17 +0200

    Fixed some aspects talked with the professor to explain better that functions shouldn't share memory because of multiple function replication in serverless. I just needed a GET request example to explain it. CRUD operations will be shown when used with a DynamoDB, where we will not need our memory to save data, and it will have persistence.

--- sections/REST/aws-lambda/src/dbManager.js
+++ sections/REST/aws-lambda/src/dbManager.js
@@ -32,47 +32,6 @@ const getAllUsers = () => {
     });
 };
 
-const getUser = (userId) => {
-    let user = users.find(e => e.userId === userId);
-    return new Promise((resolve, reject) => {
-        user ? resolve(user) : reject(`User not found!`);
-    });
-};
-
-const addUser = (user) => {
-    let added = false;
-    if(!users.filter(e => e.username === user.username).length > 0) {
-        users.push(user);
-        added = true;
-    }
-
-    return new Promise((resolve, reject) => {
-        (added) ? resolve(user) : reject("User already exists!");
-    });
-};
-
-const updateUser = (user) => {
-    let index = users.findIndex(e => e.userId === user.userId);
-    users[index] = user;
-
-    return new Promise((resolve, reject) => {
-        (index !== -1) ? resolve(user) : reject(`User ${user.username} doesn't exist!`);
-    });
-};
-
-const deleteUser = (userId) => {
-    let usersLengthBefore = users.length;
-    users = users.filter(e => e.userId !== userId);
-
-    return new Promise((resolve, reject) => {
-        (users.length < usersLengthBefore) ? resolve(user) : reject(`User doesn't exist!`);
-    });
-};
-
 module.exports = {
-    getAllUsers,
-    getUser,
-    addUser,
-    updateUser,
-    deleteUser
+    getAllUsers
 };
--- sections/REST/aws-lambda/src/index.js
+++ sections/REST/aws-lambda/src/index.js
@@ -3,31 +3,10 @@
 // dbManager file will have DynamoDB functionality in further changes. For now, it just uses mocked data to test a REST Api
 const dbManager = require('./dbManager');
 
-// uuidv1() will allow us to create a random id based in a timestamp. This will avoid the collision of id's 
-const uuidv1 = require('uuid/v1');
-
 exports.usersHandler = (event, context, callback) => {
-
     switch (event.httpMethod) {
-        // Checking the path, we can know when a GET/PUT/... event for the same aws lambda function is refered to one or another
-        // route
         case 'GET':
-            if(event.path === '/users') {
-                getAllUsers(callback);
-            } else {
-                if(event.pathParameters && event.pathParameters.username) {
-                    getUser(event.pathParameters.username, callback);
-                }
-            }
-            break;
-        case 'POST':
-            addUser(event, callback);
-            break;
-        case 'PUT':
-            updateUser(event, callback);
-            break;
-        case 'DELETE':
-            deleteUser(event, callback);
+            getAllUsers(callback);
             break;
         default:
             sendResponse(400, `Unsupported method ${event.httpMethod}`, callback);
@@ -37,63 +16,6 @@ exports.usersHandler = (event, context, callback) => {
 const getAllUsers = (callback) => {
     dbManager.getAllUsers()
     .then((res) => {
-        console.log(res);
-        sendResponse(200, res, callback);
-    })
-    .catch((err) => {
-        console.log(err);
-        sendResponse(200, err, callback);
-    });
-};
-
-const getUser = (username, callback) => {
-
-    dbManager.getUser(username)
-    .then((res) => {
-        console.log(res);
-        sendResponse(200, res, callback);
-    })
-    .catch((err) => {
-        console.log(err);
-        sendResponse(200, err, callback);
-    });
-};
-
-const addUser = (event, callback) => {
-    const user = JSON.parse(event.body);
-    user.userId = uuidv1();
-
-    dbManager.addUser(user)
-    .then((res) => {
-        console.log(res);
-        sendResponse(200, res, callback);
-    })
-    .catch((err) => {
-        console.log(err);
-        sendResponse(200, err, callback);
-    });
-};
-
-const updateUser = (event, callback) => {
-    const user = JSON.parse(event.body);
-
-    dbManager.updateUser(user)
-    .then((res) => {
-        console.log(res);
-        sendResponse(200, res, callback);
-    })
-    .catch((err) => {
-        console.log(err);
-        sendResponse(200, err, callback);
-    });
-};
-
-const deleteUser = (event, callback) => {
-    const user = JSON.parse(event.body);
-
-    dbManager.deleteUser(user.userId)
-    .then((res) => {
-        console.log(res);
         sendResponse(200, res, callback);
     })
     .catch((err) => {
