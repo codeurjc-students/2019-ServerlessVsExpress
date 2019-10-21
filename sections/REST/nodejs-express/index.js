04296e7f5177a49048025447a0bad51b014ae4e6
Francisco Robles Castro <franrobles8@gmail.com>
2019-10-21 11:11:58 +0200

    Fixed API. We just needed a GET method for this example. We will implement a full CRUD API when we use DynamoDB, to be able to persist the data.

--- sections/REST/nodejs-express/index.js
+++ sections/REST/nodejs-express/index.js
@@ -8,10 +8,6 @@ const services = require("./mockup-services");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('API route works!');
-});
-
 app.get('/users', (req, res) => {
     services.getAllUsers()
     .then((users) => {
@@ -20,100 +16,6 @@ app.get('/users', (req, res) => {
             code: 200,
             message: 'Users fetched successfully',
             data: users
-        });
-    })
-    .catch((err) => {
-        res.send({
-            error: true,
-            code: 200,
-            message: err
-        });
-    });
-    
-});
-
-app.get('/user/:username', (req, res) => {
-    services.getUser(req.params.username)
-    .then((user) => {
-        res.send({
-            error: false,
-            code: 201,
-            message: `${user.username} has been found!`,
-            data: user
-        });
-    })
-    .catch((err) => {
-        res.send({
-            error: true,
-            code: 200,
-            message: err
-        });
-    });
-});
-
-app.post('/user', (req, res) => {
-    const user = {
-        username: req.body.username,
-        password: req.body.surname,
-        name: req.body.name,
-        lastname: req.body.lastname
-    };
-
-    services.addUser(user)
-    .then((user) => {
-        res.send({
-            error: false,
-            code: 201,
-            message: `${user.username} added successfully!`
-        });
-    })
-    .catch((err) => {
-        res.send({
-            error: true,
-            code: 200,
-            message: err
-        });
-    });
-    
-});
-
-app.put('/user', (req, res) => {
-    const user = {
-        username: req.body.username,
-        password: req.body.surname,
-        name: req.body.name,
-        lastname: req.body.lastname
-    };
-
-    services.updateUser(user)
-    .then((user) => {
-        res.send({
-            error: false,
-            code: 200,
-            message: `${user.username} updated successfully!`
-        });
-    })
-    .catch((err) => {
-        res.send({
-            error: true,
-            code: 200,
-            message: err
-        });
-    });
-    
-});
-
-app.delete('/user', (req, res) => {
-    const user = {
-        username: req.body.username
-    };
-
-    services.deleteUser(user)
-    .then((user) => {
-        res.send({
-            error: false,
-            code: 200,
-            message: `${user.username} deleted successfully!`
         });
     })
     .catch((err) => {
--- sections/REST/nodejs-express/mockup-services.js
+++ sections/REST/nodejs-express/mockup-services.js
@@ -27,47 +27,6 @@ const getAllUsers = () => {
     });
 };
 
-const getUser = (username) => {
-    let user = users.find(e => e.username === username);
-    return new Promise((resolve, reject) => {
-        user ? resolve(user) : reject(`User ${username} not found!`);
-    });
-};
-
-const addUser = (user) => {
-    let added = false;
-    if(!users.filter(e => e.username === user.username).length > 0) {
-        users.push(user);
-        added = true;
-    }
-
-    return new Promise((resolve, reject) => {
-        (added) ? resolve(user) : reject("User already exists!");
-    });
-};
-
-const updateUser = (user) => {
-    let index = users.findIndex(e => e.username === user.username);
-    users[index] = user;
-
-    return new Promise((resolve, reject) => {
-        (index !== -1) ? resolve(user) : reject(`User ${user.username} doesn't exist!`);
-    });
-};
-
-const deleteUser = (user) => {
-    let usersLengthBefore = users.length;
-    users = users.filter(e => e.username !== user.username);
-
-    return new Promise((resolve, reject) => {
-        (users.length < usersLengthBefore) ? resolve(user) : reject(`User ${user.username} doesn't exist!`);
-    });
-};
-
 module.exports = {
-    getAllUsers,
-    getUser,
-    addUser,
-    updateUser,
-    deleteUser
+    getAllUsers
 };
