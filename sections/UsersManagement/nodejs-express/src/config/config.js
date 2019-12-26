5d9ab80e9262535b3f0ad59075f2ab55fbf89e58
Francisco Robles Castro <franrobles8@gmail.com>
2019-12-26 23:19:28 +0100

    Added readme for Users Management. It still needs the AWS users management readme

--- sections/UsersManagement/nodejs-express/src/config/config.js
+++ sections/UsersManagement/nodejs-express/src/config/config.js
@@ -1,6 +1,8 @@
-const MONGO_DB_USER = "franrobles8";
-const MONGO_DB_PASSWORD = "fUj0yXIwE33er99z";
+const MONGO_DB_NAME = "db_users_management";
+const MONGO_DB_USER = "mongodb_user";
+const MONGO_DB_PASSWORD = "your_mongo_db_password";
 
+// If your MongoDB is inside another cluster, the url may vary...
 module.exports = {
     SERVER_PORT: 4000,
     SECRET: "Aq.?*OxMe;",
@@ -8,5 +10,5 @@ module.exports = {
     ACCOUNT_ACTIVATION_SECRET: "aAD?!",
     ACCESS_TOKEN_EXPIRATION_TIME: "5h",
     REFRESH_TOKEN_EXPIRATION_TIME: "20d",
-    MONGO_DB_CONNECTION_URL: `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@cluster0-oampc.mongodb.net/db_users_management?retryWrites=true&w=majority`
+    MONGO_DB_CONNECTION_URL: `mongodb+srv://${MONGO_DB_USER}:${MONGO_DB_PASSWORD}@cluster0-oampc.mongodb.net/${MONGO_DB_NAME}?retryWrites=true&w=majority`
 };
