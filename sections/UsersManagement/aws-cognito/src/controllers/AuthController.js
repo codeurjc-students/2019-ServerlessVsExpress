e8fe4f376bedaea4f940da7dc21cc99cc25f6c8b
Francisco Robles Castro <franrobles8@gmail.com>
2020-01-09 21:02:21 +0100

    Added backend functionality in AWS Cognito to register a user and activate it by an email link and also to perform a login action

--- sections/UsersManagement/aws-cognito/src/controllers/AuthController.js
+++ sections/UsersManagement/aws-cognito/src/controllers/AuthController.js
@@ -3,3 +3,7 @@ const AuthService = require('../services/AuthService');
 exports.register = async (data) => {
     return await AuthService.register(data);
 }
+
+exports.login = async (data) => {
+    return await AuthService.login(data);
+}
--- sections/UsersManagement/aws-cognito/src/index.js
+++ sections/UsersManagement/aws-cognito/src/index.js
@@ -1,19 +1,33 @@
 const AuthController = require('./controllers/AuthController');
 
 exports.usersHandler = async (event, context, callback) => {
-    console.log(event);
-    console.log(context);
     switch (event.httpMethod) {
         case 'GET':
                 sendResponse(200, `Received ${event.httpMethod} method!`, callback);
                 break;
         case 'POST':
-                try{
-                    const data = JSON.parse(event.body);
-                    await AuthController.register(data);
-                    sendResponse(201, `User has been registered successfully!`, callback);
-                } catch(err) {
-                    sendResponse(err.statusCode, err.message, callback);
+                const challenge = event.path;
+
+                switch(challenge) {
+                    case '/auth/register':
+                            try{
+                                const data = JSON.parse(event.body);
+                                await AuthController.register(data);
+                                sendResponse(201, `User has been registered successfully!`, callback);
+                            } catch(err) {
+                                sendResponse(err.statusCode, err.message, callback);
+                            }
+                        break;
+                    case '/auth/login':
+                            try{
+                                const data = JSON.parse(event.body);
+                                const res = await AuthController.login(data);
+                                console.log(res);
+                                sendResponse(200, res, callback);
+                            } catch(err) {
+                                sendResponse(err.statusCode, err.message, callback);
+                            }
+                        break;
                 }
                 break;
         default:
