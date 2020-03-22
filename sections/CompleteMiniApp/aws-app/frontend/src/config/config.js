d1f938018365c61d7ff4f79f4c243f043e996b34
Francisco Robles Castro <franrobles8@gmail.com>
2020-03-22 16:31:11 +0100

    Added websockets functionality to miniapp

--- sections/CompleteMiniApp/aws-app/frontend/src/config/config.js
+++ sections/CompleteMiniApp/aws-app/frontend/src/config/config.js
@@ -1,3 +1,4 @@
-export const BASE_URL_API_BACKEND = 'https://0ie0jk9lza.execute-api.eu-west-2.amazonaws.com/dev';
-export const BASE_URL_API_CONNECTION = 'https://crcqautrk6.execute-api.eu-west-2.amazonaws.com/Prod';
-export const BASE_URL_PDF_API = 'https://sln3mdmve8.execute-api.eu-west-2.amazonaws.com/Prod';
+export const BASE_URL_API_BACKEND = 'https://66mcohe9di.execute-api.eu-west-2.amazonaws.com/dev';
+export const BASE_URL_API_CONNECTION = 'https://zejrdq5r01.execute-api.eu-west-2.amazonaws.com/Prod';
+export const BASE_URL_PDF_API = 'https://m283i3j40a.execute-api.eu-west-2.amazonaws.com/Prod';
+export const URL_WEBSOCKETS_API = 'wss://5spqvn2ikl.execute-api.eu-west-2.amazonaws.com/dev';
